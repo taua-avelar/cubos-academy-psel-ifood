428aaf26b286ec1bb82210200fffd71cf24737be
TauaAvelar <you@example.com>
2021-08-11 14:21:41 -0300

    Conversao tamanho de roupa BR > US

---
+++ tamanho-das-camisas.js
@@ -0,0 +1,18 @@
+function solucao(tamanhoBr) {
+    if (tamanhoBr == 'PP') {
+        console.log('XS')
+    }
+      else if (tamanhoBr == 'P') {
+          console.log('S')
+      }
+      else if (tamanhoBr == 'M') {
+          console.log('M')
+    }
+      else if (tamanhoBr == 'G') {
+          console.log('L')
+    }
+      else if (tamanhoBr == 'GG') {
+          console.log('XL')
+    }
+  }
+  
