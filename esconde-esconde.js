70e3b56779cc296cb02b0c365cd900c065b76b58
TauaAvelar <you@example.com>
2021-08-11 14:23:06 -0300

    Contador para numeros maiores que 20

---
+++ esconde-esconde.js
@@ -0,0 +1,11 @@
+function solucao(numero) {
+	if (numero <= 20) {
+        console.log('VOCE CONSEGUE, MANINHO')
+    }
+    else {
+        for (let count = 21; count <= numero; count ++) {
+            console.log(count)
+        }
+    }
+    
+}
